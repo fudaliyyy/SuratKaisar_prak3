9fc8bb7b095dcefc7807fe13d7e5851f91d715b3
fudaliyyy <103308132+fudaliyyy@users.noreply.github.com>
2022-04-10 12:37:31 +0700

    Update SuratKaisar.java

--- src/suratkaisar/SuratKaisar.java
+++ src/suratkaisar/SuratKaisar.java
@@ -14,7 +14,40 @@ public class SuratKaisar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Scanner sc = new Scanner(System.in);
+        String alfabet = sc.nextLine();
+        int pergeseran = sc.nextInt();
+        int panjang = alfabet.length();
+        System.out.print(pergeseran(pergeseran,alfabet,panjang));
+      }
     
-}
+    static String pergeseran(int pergeseran, String alfabet,int panjang){
+        String jadi = "";  
+        for(int a = 0;a<panjang;a++){
+       
+          int x = alfabet.charAt(a);
+          if(x == 32){
+          jadi = jadi+" ";
+          } else if (x >=65 && x<=90){
+            x = x+pergeseran;
+            if(x>90){
+              x-=26;
+              jadi = jadi+(char)x;
+          }else{
+              jadi = jadi+(char)x;
+            }
+              
+          } else if(x>=97&&x<=122){
+            x = x+pergeseran;
+            if(x>122){
+              x -=26;
+              jadi = jadi+(char)x;
+            }else{
+              jadi = jadi+(char)x;
+            }
+          } 
+        }
+        
+        return jadi;
+        }
+    }
